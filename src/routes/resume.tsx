af882ea2be40e6b38d4292cf7232614a16e2a7e8
Mitsuki Ogasahara <negiga@gmail.com>
2026-01-21 09:55:50 +0900

    feat: add blog link to resume page
    Add a link to the blog (yamitzky.dev/blog) in the resume page header
    alongside existing social links (GitHub, X, Web).

--- src/routes/resume.tsx
+++ src/routes/resume.tsx
@@ -224,10 +224,33 @@ function ResumePage() {
             </svg>
             Web
           </a>
+          <a
+            href="https://yamitzky.dev/blog"
+            target="_blank"
+            rel="noreferrer"
+            className="pill"
+          >
+            <svg
+              className="w-4 h-4"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              aria-hidden="true"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M19 20H5a2 2 0 01-2-2V6a2 2 0 012-2h10a2 2 0 012 2v1m2 13a2 2 0 01-2-2V7m2 13a2 2 0 002-2V9a2 2 0 00-2-2h-2m-4-3H9M7 16h6M7 8h6v4H7V8z"
+              />
+            </svg>
+            Blog
+          </a>
         </div>
         {/* Print only: simple contact info */}
         <p className="hidden print:block text-sm text-gray-600">
-          GitHub: github.com/yamitzky | X: @yamitzky | Web: yamitzky.dev
+          GitHub: github.com/yamitzky | X: @yamitzky | Web: yamitzky.dev | Blog:
+          yamitzky.dev/blog
         </p>
       </header>
 
